c33d5dfaab86cff5877b540ad7f26622ab45be18
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-05-11 20:14:27 +0300

    Add: filter

--- partials/Header/components/Categories.tsx
+++ partials/Header/components/Categories.tsx
@@ -5,6 +5,7 @@ import { FC } from "react";
 
 export const HeaderCategories: FC = () => {
     return <>
+        <Link href={"/shop"}><a>Все категории</a></Link>|
         {Object.keys(category_ru).map(key => (
             <Link href={assembleLink("/shop", {category: key})} key={key}><a>{category_ru[key as keyof typeof category_ru]}</a></Link>
         ))}
