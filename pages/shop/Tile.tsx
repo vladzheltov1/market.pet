47b84857aa0ea3cfc54738cd90799f53a08f2179
Vladislav <vlad.zheltov1@gmail.com>
2022-05-12 22:18:06 +0300

    Fix: shop Tiles styling

--- pages/shop/Tile.tsx
+++ pages/shop/Tile.tsx
@@ -1,5 +1,7 @@
 import { PRODUCT_NO_IMAGE } from "@/constants/paths";
+import { CURRENCY } from "@/constants/symbols";
 import { Product } from "@/types/products";
+import { addSpacesToPrice } from "@/utils/adapters";
 import Image from "next/image";
 import { FC } from "react";
 import shopStyles from "./Shop.module.scss";
@@ -11,8 +13,13 @@ export const Tile: FC<Product> = (props) => {
 
     return (
         <div className={shopStyles.product_tile}>
-            <div className={shopStyles.product_tile__title}>{product.title}</div>
-            <div className={shopStyles.product_tile__picture}><Image src={photo} layout="fill" alt="" /></div>
+            <div className={shopStyles.product_tile__picture}>
+                <Image src={photo} layout="fill" alt="" />
+            </div>
+            <div className={shopStyles.product_tile__bottom}>
+                <div className={shopStyles.product_tile__price}>{addSpacesToPrice(product.price.value)} {CURRENCY[product.price.currency]}</div>
+                <div className={shopStyles.product_tile__title}>{product.title}</div>
+            </div>
         </div>
     )
 }
