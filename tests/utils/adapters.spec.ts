9868fd777cc2a90af2545cfb2bfe457cd894c01e
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-05-22 20:47:39 +0300

    Add: new test cases

--- tests/utils/adapters.spec.ts
+++ tests/utils/adapters.spec.ts
@@ -5,6 +5,8 @@ import { addSpacesToPrice } from "@/utils/adapters";
 describe("test utils/adapters", () => {
     it("should convert number to easily readable string with spaces every 3 symbols", () => {
         expect(addSpacesToPrice(100000000)).toBe("100 000 000");
+        expect(addSpacesToPrice(10000000)).toBe("10 000 000");
+        expect(addSpacesToPrice(1000000)).toBe("1 000 000");
         expect(addSpacesToPrice(100000)).toBe("100 000");
         expect(addSpacesToPrice(100)).toBe("100");
         expect(addSpacesToPrice(10)).toBe("10");
