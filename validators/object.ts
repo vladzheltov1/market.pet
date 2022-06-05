896d1a4340dceaf019e1867b3a537a317532f49b
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-06-05 10:18:07 +0300

    Fix: wrong validation

--- validators/object.ts
+++ validators/object.ts
@@ -1,3 +1,5 @@
+import { isArray } from "./array";
+
 export const isObject = (value: any): boolean => (typeof value === "object") && (value !== null) && (Array.isArray(value) === false);
 
-export const isEmptyObject = (object: object): boolean => Object.keys(object).length === 0;
+export const isEmptyObject = (object: object): boolean => !isArray(object) && Object.keys(object).length === 0;
